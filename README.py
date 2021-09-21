8a768a71f0073c81cb9578158d217c84855f14ae
AlpinXCODE <91122025+AlpinXCODE@users.noreply.github.com>
2021-09-21 16:25:40 +0700

    Update README.py

--- README.py
+++ README.py
@@ -1,1 +1,13 @@
 Jangan LuPa Pollow
+
+• pkg update
+• pkg upgrade
+• pkg install bash
+• pkg install bs4
+• pkg install python2
+• pip2 install requests
+• pip2 install mechanize
+• pkg install git
+• git clone https://github.com/AlpinXCODE/kangcode
+• cd kangcode
+• python2 run.py
